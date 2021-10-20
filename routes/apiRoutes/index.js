c2eeca41014ccbd1337234fa206631b34c8665ae
Kyrsten <ksurbayan@gmail.com>
2021-10-20 13:45:55 -0800

    saves new note to db.json

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const { notes } = require('../../db/db.json');
+const notes = require('../../db/db');
+const createNote = require('../../lib/notes');
 
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
-    
+router.post('/notes', (req, res) => {
+    const note = createNote(req.body, notes);
+    res.json(note);
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 const fs = require('fs');
 const path = require('path');
-const { notes } = require('./db/db.json');
+const notes = require('./db/db.json');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
