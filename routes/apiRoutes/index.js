0107140306f9539e8fdb4adfb7e0e0663273cbad
Kyrsten <ksurbayan@gmail.com>
2021-10-20 17:00:44 -0800

    deletes notes by id

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const notes = require('../../db/db');
-const createNote = require('../../lib/notes');
+const { createNote, deleteNote } = require('../../lib/notes');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
@@ -9,6 +9,11 @@ router.get('/notes', (req, res) => {
 router.post('/notes', (req, res) => {
     const note = createNote(req.body, notes);
     res.json(note);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id, notes);
+    res.json(notes);
 })
 
 module.exports = router;
